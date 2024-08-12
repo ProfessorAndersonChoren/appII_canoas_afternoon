5639b580e6675251ca105d65d266a89e1dd064d1
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2024-08-12 17:01:55 -0300

    Dart - Variaveis e constantes

---
+++ aula_1/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Variáveis
+  var nome = "Anderson";
+  String sobrenome = "Choren";
+  //nome = 45; // Tentando mudar o tipo da variável
+  nome = "João";
+
+  // Constantes
+  const signo = "Virgem";
+  //signo = "Aquário";
+  const String texto = "Um texto qualquer";
+
+  print(nome);
+  print(texto);
+}
